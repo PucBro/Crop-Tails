96720baff8f59fcec25408de3d88d2756bb1a3fd
yodakus <yodak1991@gmail.com>
2026-05-20 23:43:36 -0600

    dialogues added

--- scenes/ui/tools_panel.gd
+++ scenes/ui/tools_panel.gd
@@ -6,7 +6,15 @@ extends PanelContainer
 @onready var tool_tomato: Button = $MarginContainer/HBoxContainer/ToolTomato
 
 
-
+func _ready() -> void:
+	ToolManager.enable_tool.connect(on_enable_tool_button)
+	
+	
+	tool_tilling.disabled = true
+	tool_axe.disabled = true
+	tool_tomato.disabled= true
+	tool_corn.disabled = true
+	tool_watering_can.disabled = true
 	
 	
 	
@@ -53,3 +61,15 @@ func _on_tool_tomato_toggled(toggled_on: bool) -> void:
 		ToolManager.select_tool(DataTypes.Tools.PlantTomato)
 	else:
 		ToolManager.select_tool(DataTypes.Tools.None)
+		
+func on_enable_tool_button(tool: DataTypes.Tools)-> void:
+	if tool == DataTypes.Tools.TillGround:
+		tool_tilling.disabled = false
+	elif tool == DataTypes.Tools.WaterCrops:
+		tool_watering_can.disabled = false
+	elif tool == DataTypes.Tools.AxeWood:
+		tool_axe.disabled = false
+	elif tool == DataTypes.Tools.PlantCorn:
+		tool_corn.disabled = false
+	elif tool == DataTypes.Tools.PlantTomato:
+		tool_tomato.disabled = false
